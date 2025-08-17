e16878d2e4e6d8ba2e0d34bfeda721adaec96fb9
dayne-urahara <mokhliaz@gmail.com>
2025-08-17 22:52:22 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE = "budget-v1";
+const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./app.js"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then(
+      (r) =>
+        r ||
+        fetch(event.request)
+          .then((resp) => {
+            const clone = resp.clone();
+            caches.open(CACHE).then((c) => c.put(event.request, clone));
+            return resp;
+          })
+          .catch(() => caches.match("./"))
+    )
+  );
+});
